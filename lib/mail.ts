e189165a519ccd64a5d82c83b26974567aae7f7e
Tiago Santos <a62082@alunos.ipb.pt>
2026-07-22 03:18:25 +0100

    Update: Segurança SMTP false para true.

--- lib/mail.ts
+++ lib/mail.ts
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer";
 export const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: false,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
