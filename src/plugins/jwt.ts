574d8b3e729afa8073cbe826a6e2509a733b7468
Tiago Santos <a62082@alunos.ipb.pt>
2026-07-22 03:01:50 +0100

    Fix: Configuração do dotenv em plugin/jwt.

--- src/plugins/jwt.ts
+++ src/plugins/jwt.ts
@@ -1,9 +1,16 @@
 import fp from "fastify-plugin";
 import fastifyJwt from "@fastify/jwt";
+import { AppError } from "../errors/app.error";
 
 export default fp(async (app) => {
+  const jwtSecret = process.env.JWT_SECRET;
+
+  if (!jwtSecret) {
+    throw new AppError("JWT_SECRET não está configurado", 500);
+  }
+
   await app.register(fastifyJwt, {
-    secret: process.env.JWT_SECRET!,
+    secret: jwtSecret,
     sign: {
       expiresIn: "1d",
     },
